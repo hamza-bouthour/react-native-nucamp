390c435b5963b9e7e5ed14d5975b81631cde178b
Hamza Bouthour <bouthour.h@gmail.com>
2021-03-18 15:52:52 -0700

    more animations added

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { ScrollView } from 'react-native-gesture-handler';
 import { createStackNavigator } from 'react-navigation-stack';
 import { Card, ListItem, Button, Icon, Text } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 // const ContactNavigator = createStackNavigator(
 //     {
 //         Contact: { screen: Contact}
@@ -15,22 +16,24 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-                <Card
-                title="Contact Informations"
-                wrapperStyle={{margin: 20}}
-                >
-                  <Text 
-                  style={{margin: 20}}>     
-                    1 Nucamp Way
-                    Seattle, WA 98001
-                    U.S.A.
-                  </Text>
-                  <Text 
-                  style={{margin: 20}}>
-                        Phone: 1-206-555-1234
-                        Email: campsites@nucamp.co
-                  </Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card
+                    title="Contact Informations"
+                    wrapperStyle={{margin: 20}}
+                    >
+                    <Text 
+                    style={{margin: 20}}>     
+                        1 Nucamp Way
+                        Seattle, WA 98001
+                        U.S.A.
+                    </Text>
+                    <Text 
+                    style={{margin: 20}}>
+                            Phone: 1-206-555-1234
+                            Email: campsites@nucamp.co
+                    </Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         )
     }
